86597664402824566f978bbe821c1441451d14ab
chidindu327 <chidindueze@gmail.com>
2024-10-23 19:17:53 +0100

    chats
    done

--- html/ltr/nav.php
+++ html/ltr/nav.php
@@ -4,64 +4,12 @@
             <i class="mdi mdi-dots-horizontal"></i>
             <span class="hide-menu">Personal</span>
         </li>
-        
-
-
-        <li class="sidebar-item">
-            <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                <i class="mdi mdi-gradient"></i>
-                <span class="hide-menu">Extra </span>
-            </a>
             <ul aria-expanded="false" class="collapse  first-level">
                 <li class="sidebar-item">
                     <a href="chats.php" class="sidebar-link">
                         <i class="ti-comment"></i>
                         <span class="hide-menu"> Chats Apps </span>
                     </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                        <i class="mdi mdi-account-circle"></i>
-                        <span class="hide-menu">Authentication</span>
-                    </a>
-                    <ul aria-expanded="false" class="collapse first-level">
-                        <li class="sidebar-item">
-                            <a href="login1.php" class="sidebar-link">
-                                <i class="mdi mdi-account-key"></i>
-                                <span class="hide-menu"> Login </span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="login2.php" class="sidebar-link">
-                                <i class="mdi mdi-account-key"></i>
-                                <span class="hide-menu"> Login 2 </span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="authentication-register1.html" class="sidebar-link">
-                                <i class="mdi mdi-account-plus"></i>
-                                <span class="hide-menu"> Register</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="authentication-register2.html" class="sidebar-link">
-                                <i class="mdi mdi-account-plus"></i>
-                                <span class="hide-menu"> Register 2</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="authentication-lockscreen.html" class="sidebar-link">
-                                <i class="mdi mdi-account-off"></i>
-                                <span class="hide-menu"> Lockscreen</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="authentication-recover-password.html" class="sidebar-link">
-                                <i class="mdi mdi-account-convert"></i>
-                                <span class="hide-menu"> Recover password</span>
-                            </a>
-                        </li>
-                    </ul>
                 </li>
 
             </ul>
